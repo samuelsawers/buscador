2a370db45445ca84c4a885fbe55edd14a6677f61
SamuelC <samuel@Samuel.sawers.com.bo>
2016-01-21 11:15:06 -0400

    [EDT]

--- youtube.py
+++ youtube.py
@@ -13,8 +13,7 @@ class Youtube:
 			video = yt.filter(codec)[0]
 			video.download(location, on_progress=utils.print_status)
 		except Exception, e:
-			print e
-			print "Error: Invalid URL or network problem"
+			print "Error: Invalid URL or network problem or file exist"
 
 	def get_files(self, url):
 		position_link = 0
@@ -32,6 +31,7 @@ class Youtube:
 					valor = (video_url_preffix+valor).split('&')[0]
 					print valor
 					self.download_now(str(valor), 'mp4', '/Users/samuel/Documents/python')
+					print '\n'
 				print 'se descargo la lista el archivo'
 			else: print 'link de youtube no hace referencia a un archivo valido'
 
@@ -42,13 +42,13 @@ class Youtube:
 			valor = (url).split('&')[0]
 			print valor
 			self.download_now(str(valor), 'mp4', '/Users/samuel/Documents/python')
-			print 'se descargo el video'
+			print '\nse descargo el video'
 		else: print 'link de youtube no hace referencia a un archivo valido'
 
 
 # Muestra una barra de progreso que se actualiza en una misma linea. Ejemplo de uso:
 youtube = Youtube()
 #url = 'https://www.youtube.com/watch?v=qEvnwW-YtRw&list=PLOcurmo_gZqZPgMdgj_J14IH5VQLkXY_9'
-url = 'https://www.youtube.com/watch?v=fRh_vgS2dFE&index=4&list=PLSFitF4B6yNS82pcRx5XvD1PB6m8lIs5J'
-#url = raw_input('ingrese url de youtube:\n')
+#url = 'https://www.youtube.com/watch?v=fRh_vgS2dFE&index=4&list=PLSFitF4B6yNS82pcRx5XvD1PB6m8lIs5J'
+url = raw_input('ingrese url de youtube:\n')
 youtube.get_file(url)
